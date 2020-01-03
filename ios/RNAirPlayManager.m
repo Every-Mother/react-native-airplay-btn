9c7fb50eac861b841cf98f603aad95cc85b65297
bsqu <briansquashic@gmail.com>
2020-01-03 09:45:44 -0500

    customized airplay btn color

--- ios/RNAirPlayManager.m
+++ ios/RNAirPlayManager.m
@@ -13,11 +13,21 @@ RCT_EXPORT_MODULE();
 @synthesize bridge = _bridge;
 
 - (UIView *)view {
-
     MPVolumeView *volumeView = [[MPVolumeView alloc] init];
+
+    for (UIView *wnd in volumeView.subviews) {
+      if ([wnd isKindOfClass:[UIButton class]]) {
+          UIButton *button = (UIButton*) wnd;
+          UIImage *img = button.currentImage;
+          UIImage *img2 = [img imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+          [volumeView setRouteButtonImage: img2 forState:UIControlStateNormal];
+          break;
+      }
+    }
+
+    volumeView.tintColor = [UIColor colorWithRed: 0.69 green:0.41 blue:0.36 alpha:1.0];
     volumeView.showsVolumeSlider = false;
 
     return volumeView;
-
 }
 @end
